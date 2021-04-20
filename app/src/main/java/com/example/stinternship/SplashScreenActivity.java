1f1d424d425c6e6e2d58df4bc32e110bdd637b0a
Dibyaroopdas <59794247+Dibyaroopdas@users.noreply.github.com>
2021-04-20 15:38:05 +0530

    Second-Commit

--- app/src/main/java/com/example/stinternship/SplashScreenActivity.java
+++ app/src/main/java/com/example/stinternship/SplashScreenActivity.java
@@ -2,13 +2,33 @@ package com.example.stinternship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ProgressBar;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+
+    int SPLASH_TIME = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mySuperIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(mySuperIntent);
+
+                finish();
+            }
+        }, SPLASH_TIME);
     }
+
+
 }
